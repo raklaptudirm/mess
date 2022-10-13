aa06d8e7e88400598b89dc18aecf88c3aac5d596
Rak Laptudirm <raklaptudirm@gmail.com>
2022-10-13 17:03:15 +0000

    chore: use stdlib function to reverse bitboard
    Improvement speeds up perft to 10 Mn/s.

--- pkg/attacks/sliding.go
+++ pkg/attacks/sliding.go
@@ -1,6 +1,8 @@
 package attacks
 
 import (
+	"math/bits"
+
 	"laptudirm.com/x/mess/pkg/board/bitboard"
 	"laptudirm.com/x/mess/pkg/square"
 )
@@ -12,10 +14,5 @@ func hyperbola(s square.Square, occ, mask bitboard.Board) bitboard.Board {
 }
 
 func reverse(b bitboard.Board) bitboard.Board {
-	b = (b&0x5555555555555555)<<1 | ((b >> 1) & 0x5555555555555555)
-	b = (b&0x3333333333333333)<<2 | ((b >> 2) & 0x3333333333333333)
-	b = (b&0x0f0f0f0f0f0f0f0f)<<4 | ((b >> 4) & 0x0f0f0f0f0f0f0f0f)
-	b = (b&0x00ff00ff00ff00ff)<<8 | ((b >> 8) & 0x00ff00ff00ff00ff)
-
-	return (b << 48) | ((b & 0xffff0000) << 16) | ((b >> 16) & 0xffff0000) | (b >> 48)
+	return bitboard.Board(bits.Reverse64(uint64(b)))
 }
