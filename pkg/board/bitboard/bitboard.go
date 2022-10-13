94cbf6c50763b3939bf1909f318ed5c7b05eb998
Rak Laptudirm <raklaptudirm@gmail.com>
2022-10-13 17:01:38 +0000

    chore: use  stdlib function to get first set bit
    Improvement speeds up perft to 8 Mn/s.

--- pkg/board/bitboard/bitboard.go
+++ pkg/board/bitboard/bitboard.go
@@ -15,7 +15,11 @@
 // functions for manipulating them.
 package bitboard
 
-import "laptudirm.com/x/mess/pkg/square"
+import (
+	"math/bits"
+
+	"laptudirm.com/x/mess/pkg/square"
+)
 
 // Board is a 64-bit bitboard
 type Board uint64
@@ -61,20 +65,8 @@ func (b *Board) Pop() square.Square {
 	return sq
 }
 
-var index = [square.N]square.Square{
-	0, 47, 1, 56, 48, 27, 2, 60,
-	57, 49, 41, 37, 28, 16, 3, 61,
-	54, 58, 35, 52, 50, 42, 21, 44,
-	38, 32, 29, 23, 17, 11, 4, 62,
-	46, 55, 26, 59, 40, 36, 15, 53,
-	34, 51, 20, 43, 31, 22, 10, 45,
-	25, 39, 14, 33, 19, 30, 9, 24,
-	13, 18, 8, 12, 7, 6, 5, 63,
-}
-
 func (b Board) FirstOne() square.Square {
-	const deBruijn = 0x03f79d71b4cb0a89
-	return index[((b^(b-1))*deBruijn)>>58]
+	return square.Square(bits.TrailingZeros64(uint64(b)))
 }
 
 // IsSet checks whether the given Square is set in the bitboard.
