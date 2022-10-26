68218ff689f18b99e05eca15e09a4befcb5d185e
Rak Laptudirm <raklaptudirm@gmail.com>
2022-10-26 17:56:44 +0000

    chore: add copyright notices

--- pkg/chess/mailbox/mailbox.go
+++ pkg/chess/mailbox/mailbox.go
@@ -1,4 +1,4 @@
-// Copyright © 2022 Rak Laptudirm <raklaptudirm@gmail.com>
+// Copyright © 2022 Rak Laptudirm <rak@laptudirm.com>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
--- pkg/zobrist/zobrist.go
+++ pkg/zobrist/zobrist.go
@@ -1,5 +1,18 @@
 package zobrist
 
+// Copyright © 2022 Rak Laptudirm <rak@laptudirm.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import (
 	"laptudirm.com/x/mess/pkg/chess/move/castling"
 	"laptudirm.com/x/mess/pkg/chess/piece"
@@ -16,7 +29,7 @@ var SideToMove Key
 
 func init() {
 	var rng util.PRNG
-	rng.Seed(1070372)
+	rng.Seed(1070372) // seed used from Stockfish
 
 	// piece square numbers
 	for p := 0; p < piece.N; p++ {
