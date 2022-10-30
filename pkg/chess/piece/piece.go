516e5bae54149e440b91a7b71f456ec34496c8a5
Rak Laptudirm <raklaptudirm@gmail.com>
2022-10-30 11:24:28 +0000

    chore: refactor chess/piece

--- pkg/chess/piece/piece.go
+++ pkg/chess/piece/piece.go
@@ -22,47 +22,9 @@
 // colors respectively.
 package piece
 
-// NewColor creates an instance of color from the given id.
-func NewColor(id string) Color {
-	switch id {
-	case "w":
-		return White
-	case "b":
-		return Black
-	default:
-		panic("new color: invalid color id")
-	}
-}
-
-// Color represents the color of a Piece
-type Color int
-
-// various piece colors
-const (
-	White Color = iota
-	Black
-
-	NColor = 2
-)
-
-func (c Color) Other() Color {
-	return c ^ Black
-}
-
-// String converts a Color to it's string representation.
-func (c Color) String() string {
-	switch c {
-	case Black:
-		return "b"
-	case White:
-		return "w"
-	default:
-		panic("new color: invalid color id")
-	}
-}
-
+// New creates a new Piece with the given type and color.
 func New(t Type, c Color) Piece {
-	return Piece(c<<3) + Piece(t)
+	return Piece(c<<colorOffset) | Piece(t)
 }
 
 // NewFromString creates an instance of Piece from the given piece id.
@@ -97,31 +59,15 @@ func NewFromString(id string) Piece {
 	}
 }
 
-type Type int
+// Piece represents a colored chess piece.
+// Format: MSB [color 1 bit][type 3 bits] LSB
+type Piece uint8
 
-// various chess pieces
-const (
-	NoType Type = iota
-	Pawn
-	Knight
-	Bishop
-	Rook
-	Queen
-	King
-
-	NType = 7
-)
-
-func (t Type) String() string {
-	return Piece(t | 8).String()
-}
-
-// Piece represents a chess piece.
-type Piece int
-
+// constants representing colored chess pieces
 const (
 	NoPiece Piece = 0
 
+	// white pieces
 	WhitePawn   Piece = Piece(Pawn)
 	WhiteKnight Piece = Piece(Pawn) + 1
 	WhiteBishop Piece = Piece(Pawn) + 2
@@ -129,39 +75,47 @@ const (
 	WhiteQueen  Piece = Piece(Pawn) + 4
 	WhiteKing   Piece = Piece(Pawn) + 5
 
+	// black pieces
 	BlackPawn   Piece = Piece(Pawn) + 8
 	BlackKnight Piece = Piece(Pawn) + 9
 	BlackBishop Piece = Piece(Pawn) + 10
 	BlackRook   Piece = Piece(Pawn) + 11
 	BlackQueen  Piece = Piece(Pawn) + 12
 	BlackKing   Piece = Piece(Pawn) + 13
-
-	N = 16
 )
 
-var Promotions = [...]Type{
-	Queen, Rook, Bishop, Knight,
+// N is the number of chess piece-color combinations there are. Ideally it
+// should be 6x2 = 12, but the number is bloated due to separating the bit
+// offsets of piece type and color to make getting them easier.
+const N = 16
+
+// constants representing field offsets in Piece
+const (
+	colorOffset = 3
+	typeMask    = (1 << colorOffset) - 1
+)
+
+var pieceToStr = [...]string{
+	NoPiece:     " ",
+	WhitePawn:   "P",
+	WhiteKnight: "N",
+	WhiteBishop: "B",
+	WhiteRook:   "R",
+	WhiteQueen:  "Q",
+	WhiteKing:   "K",
+	BlackPawn:   "p",
+	BlackKnight: "n",
+	BlackBishop: "b",
+	BlackRook:   "r",
+	BlackQueen:  "q",
+	BlackKing:   "k",
 }
 
-// String converts a Piece into it's string representation.
+// String converts a Piece into it's string representation. THe pieces are
+// represented using their standard alphabets, with white pieces having
+// upper case letters and black pieces having lower case ones.
 func (p Piece) String() string {
-	pieces := [...]string{
-		NoPiece:     " ",
-		WhitePawn:   "P",
-		WhiteKnight: "N",
-		WhiteBishop: "B",
-		WhiteRook:   "R",
-		WhiteQueen:  "Q",
-		WhiteKing:   "K",
-		BlackPawn:   "p",
-		BlackKnight: "n",
-		BlackBishop: "b",
-		BlackRook:   "r",
-		BlackQueen:  "q",
-		BlackKing:   "k",
-	}
-
-	return pieces[p]
+	return pieceToStr[p]
 }
 
 // Type returns the piece type of the given Piece.
@@ -170,7 +124,7 @@ func (p Piece) Type() Type {
 	case p == NoPiece:
 		return NoType
 	default:
-		return Type(p & 7)
+		return Type(p & typeMask)
 	}
 }
 
@@ -180,7 +134,7 @@ func (p Piece) Color() Color {
 		panic("color of piece: can't find color of NoPiece")
 	}
 
-	return Color(p >> 3)
+	return Color(p >> colorOffset)
 }
 
 // Is checks if the type of the given Piece matches the given type.
@@ -193,4 +147,35 @@ func (p Piece) Is(target Type) bool {
 func (p Piece) IsColor(target Color) bool {
 	c := p.Color()
 	return c == target
+}
+
+// Type represents the type/kind of chess piece.
+type Type uint8
+
+// constants representing chess piece types
+const (
+	NoType Type = iota
+	Pawn
+	Knight
+	Bishop
+	Rook
+	Queen
+	King
+)
+
+// NType is the number of chess piece types, including NoType.
+const NType = 7
+
+var typeToStr = [...]string{
+	NoType: "-",
+	Pawn:   "p",
+	Knight: "k",
+	Bishop: "b",
+	Rook:   "r",
+	Queen:  "q",
+	King:   "k",
+}
+
+func (t Type) String() string {
+	return typeToStr[t]
 }
