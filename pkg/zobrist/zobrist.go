7b9ba70202c3dc8155884a07520de81e99adaac8
Rak Laptudirm <raklaptudirm@gmail.com>
2022-11-03 15:01:06 +0000

    feat: zobrist keys generated at compile time

--- pkg/zobrist/zobrist.go
+++ pkg/zobrist/zobrist.go
@@ -1,5 +1,3 @@
-package zobrist
-
 // Copyright © 2022 Rak Laptudirm <rak@laptudirm.com>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,41 +11,8 @@ package zobrist
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import (
-	"laptudirm.com/x/mess/pkg/chess/move/castling"
-	"laptudirm.com/x/mess/pkg/chess/piece"
-	"laptudirm.com/x/mess/pkg/chess/square"
-	"laptudirm.com/x/mess/pkg/util"
-)
+package zobrist
+
+//go:generate go run laptudirm.com/x/mess/pkg/generator/zobrist
 
 type Key uint64
-
-var PieceSquare [piece.N][square.N]Key
-var EnPassant [square.FileN]Key
-var Castling [castling.N]Key
-var SideToMove Key
-
-func init() {
-	var rng util.PRNG
-	rng.Seed(1070372) // seed used from Stockfish
-
-	// piece square numbers
-	for p := 0; p < piece.N; p++ {
-		for s := square.A8; s <= square.H1; s++ {
-			PieceSquare[p][s] = Key(rng.Uint64())
-		}
-	}
-
-	// en passant file numbers
-	for f := square.FileA; f <= square.FileH; f++ {
-		EnPassant[f] = Key(rng.Uint64())
-	}
-
-	// castling right numbers
-	for r := castling.NoCasl; r <= castling.All; r++ {
-		Castling[r] = Key(rng.Uint64())
-	}
-
-	// black to move number
-	SideToMove = Key(rng.Uint64())
-}
