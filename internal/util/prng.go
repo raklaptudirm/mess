fa2f32603fce3b5c7c9d5609c5b970fbfb1b7ffb
Rak Laptudirm <rak@laptudirm.com>
2022-12-30 18:03:08 +0530

    chore: use golangci-lint directives instead of staticcheck

--- internal/util/prng.go
+++ internal/util/prng.go
@@ -5,15 +5,16 @@ package util
 // to the public domain by Sebastiano Vigna (2014).
 // It has the following characteristics:
 //
-//  -  Outputs 64-bit numbers
-//  -  Passes Dieharder and SmallCrush test batteries
-//  -  Does not require warm-up, no zeroland to escape
-//  -  Internal state is a single 64-bit integer
-//  -  Period is 2^64 - 1
-//  -  Speed: 1.60 ns/call (Core i7 @3.40GHz)
+//   - Outputs 64-bit numbers
+//   - Passes Dieharder and SmallCrush test batteries
+//   - Does not require warm-up, no zeroland to escape
+//   - Internal state is a single 64-bit integer
+//   - Period is 2^64 - 1
+//   - Speed: 1.60 ns/call (Core i7 @3.40GHz)
 //
 // For further analysis see
-//   <http://vigna.di.unimi.it/ftp/papers/xorshift.pdf>
+//
+//	<http://vigna.di.unimi.it/ftp/papers/xorshift.pdf>
 type PRNG struct {
 	seed uint64
 }
@@ -39,6 +40,7 @@ func (p *PRNG) Uint64() uint64 {
 func (p *PRNG) SparseUint64() uint64 {
 	// bitwise and three pseudo-random uint64s together
 	// only the bits set in all three are set in the result
-	//lint:ignore SA4000 ; Uint64 is an impure function
+
+	//nolint:staticcheck // Uint64 is an impure function
 	return p.Uint64() & p.Uint64() & p.Uint64()
 }
