0a123d43f690fae576f48b05725baef2b82be86f
Rak Laptudirm <rak@laptudirm.com>
2023-02-04 14:54:15 +0530

    chore: increase bench depth as engine is faster

--- internal/engine/cmd/bench.go
+++ internal/engine/cmd/bench.go
@@ -95,7 +95,7 @@ func NewBench(engine *context.Engine) cmd.Command {
 
 	// search limits
 	limits := search.Limits{
-		Depth:    9, // make this higher as engine gets faster
+		Depth:    12, // make this higher as engine gets faster
 		Infinite: true,
 		Time:     &time.MoveManager{Duration: math.MaxInt32},
 	}
